a706bba7ade38c9455f33f7173611a0af4ace854
DBuckley0126 <dannyboy.msn@hotmail.com>
2019-10-30 10:41:39 +0000

    Adjust some of the setup files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,9 @@ class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
-    set :views, 'app/views'
+    set :views, Proc.new { File.join(root, "../views/") }
+    register Sinatra::ActiveRecordExtension
+    set :session_secret, "my_application_secret"
   end
 
   get "/" do
