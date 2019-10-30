75e45776cee4244558394dfa7df08d8bc036762d
DBuckley0126 <dannyboy.msn@hotmail.com>
2019-10-30 16:39:00 +0000

    Full CRUD and RESTful routes implimented

--- app/controllers/remember_controller.rb
+++ app/controllers/remember_controller.rb
@@ -18,13 +18,55 @@ class RememberController < ApplicationController
     end
   end
 
-  post '/remember' do
+  post '/remembers' do
     if Helpers.is_logged_in?(session)
       @user = Helpers.current_user(session)
       @remember = Remember.new(params)
       @remember.user = @user
       @remember.save
       redirect '/remembers'
+    else
+      redirect '/signup'
+    end
+  end
+
+  get '/remembers/:id/edit' do
+    if Helpers.is_logged_in?(session)
+      @user = Helpers.current_user(session)
+      @remember = Remember.find_by(user_id: @user.id, id: params[:id])
+      erb :'/remembers/edit'
+    else
+      redirect '/signup'
+    end
+  end
+
+  get '/remembers/:id' do
+    if Helpers.is_logged_in?(session)
+      @user = Helpers.current_user(session)
+      @remember = Remember.find_by(user_id: @user.id, id: params[:id])
+      erb :'remembers/view'
+    else
+      redirect '/signup'
+    end
+  end
+
+  delete '/remembers/:id' do
+    if Helpers.is_logged_in?(session)
+      @user = Helpers.current_user(session)
+      @remember = Remember.find_by(user_id: @user.id, id: params[:id])
+      @remember.destroy
+      redirect '/remembers'
+    else
+      redirect '/signup'
+    end
+  end
+
+  patch '/remembers/:id' do
+    if Helpers.is_logged_in?(session)
+      @user = Helpers.current_user(session)
+      @remember = Remember.find_by(user_id: @user.id, id: params[:id])
+      @remember.update(params["remember"])
+      redirect "/remembers/#{@remember.id}"
     else
       redirect '/signup'
     end
