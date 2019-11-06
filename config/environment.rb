50c86546b282a4271ffd99082f54206a6401e939
DBuckley0126 <dannyboy.msn@hotmail.com>
2019-11-06 04:35:56 +0000

    Add Procfile

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ Bundler.require(:default, ENV['SINATRA_ENV'])
 
 #db_config = YAML.load_file('config/database.yml')
 #ActiveRecord::Base.establish_connection(db_config['production']
-ActiveRecord::Base.establish_connection('postgres://localhost/mydb')
+
 
 
 require 'pry'
