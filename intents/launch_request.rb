a8159c0e7d129649fb1cac03e2037d900962ca1f
DBuckley0126 <dannyboy.msn@hotmail.com>
2019-11-15 02:36:27 +0000

    Changed Alexa responce to launch request

--- intents/launch_request.rb
+++ intents/launch_request.rb
@@ -10,8 +10,8 @@ intent "LaunchRequest" do
   when "alexa connected account"
     ask("Welcome back #{user.first_name}, to Remember Me. What would you like to do this visit?")
   when "account linked"
-    tell("Welcome, #{user.first_name} to Remember Me. Your existing Remember Me account has been linked. You can tell me to remember anything. For example, you can say ‘remember where I left my keys with the answer in the kitchen draw’. So when you are struggling to remember where your keys are, simply ask me ‘do you know where I left my keys’.")
+    ask("Welcome, #{user.first_name} to Remember Me. Your existing Remember Me account has been linked. You can tell me to remember anything. For example, you can say ‘remember where I left my keys with the answer in the kitchen draw’. So when you are struggling to remember where your keys are, simply ask me ‘do you know where I left my keys’.")
   when "account created"
-    tell("Welcome, #{user.first_name} to the Remember Me skill. You can ask me to Remember anything for you so when your struggling to remember a certain something, just ask me about it! An account has also been made and you can now login to the Remember Me website to visit your dashboard.")
+    ask("Welcome, #{user.first_name} to the Remember Me skill. You can ask me to Remember anything for you so when your struggling to remember a certain something, just ask me about it! An account has also been made and you can now login to the Remember Me website to visit your dashboard. Say 'help' for more infomation on how to use this skill.")
   end
 end
